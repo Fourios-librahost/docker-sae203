ca48eee9eb9fea38ca4337138445be685575fb90
Théo <theo.poret05@gmail.com>
2024-04-16 18:21:37 +0200

    Fix network

--- exercice5a/ClientPuissance4.java
+++ exercice5a/ClientPuissance4.java
@@ -2,12 +2,13 @@ import java.io.*;
 import java.net.*;
 
 public class ClientPuissance4 {
-	private static final String SERVER_ADDRESS = "localhost";
+	private static final String SERVER_ADDRESS = "192.168.1.104";
 	private static final int PORT = 5000;
 
 	public static void main(String[] args) {
 		try {
-			Socket socket = new Socket(SERVER_ADDRESS, PORT);
+			InetAddress hostName = InetAddress.getByName(SERVER_ADDRESS);
+			Socket socket = new Socket(hostName, PORT);
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			//ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
